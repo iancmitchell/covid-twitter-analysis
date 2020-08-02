d654b6799cf98bfa15d772049b9fb75425f564c9
Ian Mitchell <iancharlesmitchell@gmail.com>
2020-08-02 19:05:07 -0400

    start search_hashtag method with successful test

--- tests/twitter_test.py
+++ tests/twitter_test.py
@@ -16,5 +16,8 @@ class TestTwitter(unittest.TestCase):
     def test_init(self):
         self.assertIsInstance(self.client, TwitterClient)
 
+    def test_search_hashtag(self):
+        self.client.search_hashtag("mask")    
+
 if __name__ == "__main__":
     unittest.main()        
--- twitter_client/__init__.py
+++ twitter_client/__init__.py
@@ -8,3 +8,7 @@ class TwitterClient(object):
 
     def _init_client(self, config) -> twitter.Api:
         return twitter.Api(consumer_key=config.get('apiKey'), consumer_secret=config.get('apiSecret'), access_token_key=config.get('accessToken'), access_token_secret=config.get('accessTokenSecret'))
+
+    def search_hashtag(self, hashtag: str):
+        tweet_data = self.client.GetSearch(raw_query=f"q=%23{hashtag}")
+        print(tweet_data)
